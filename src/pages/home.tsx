66959e6d80220d007fb168a00ca6bdb5dc5f7789
kian-m <moghtaderik@gmail.com>
2024-04-27 22:38:35 -0500

    adds speed insights

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
 import Landing from './landing';
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Home() {
   return (
     <div>
       <Landing />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
