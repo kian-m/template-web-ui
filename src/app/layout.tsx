515f1c84f4cb77de8789956716281b181ad2a3d2
kian-m <moghtaderik@gmail.com>
2024-12-29 01:05:49 -0600

    adds meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
+
+export const metadata = {
+  title: 'My Health Portal',
+  description: 'Fitness, Food & Rest'
+};
 export default function RootLayout({
   children,
 }: {
@@ -11,6 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <meta property="og:image" content="/img.webp" />
+      <link
+          rel="icon"
+          sizes="16x16 32x32 64x64"
+          href="/img.webp"
+          type="image/png"
+      />
     </html>
   );
 }
