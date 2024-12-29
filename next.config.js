362c3903bf37f4f414e18913d65e1809c87ea94c
kian-m <moghtaderik@gmail.com>
2024-12-29 01:15:31 -0600

    experimental search params

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { transpilePackages: ['@vercel/analytics'] };
+const nextConfig = {
+  transpilePackages: ['@vercel/analytics'],
+  experimental: {
+      //TODO: Remove this when the issue is fixed
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 module.exports = nextConfig;
