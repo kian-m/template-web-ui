cbe44e729eab656b71003896312e0cc2fbe02e55
kian-m <moghtaderik@gmail.com>
2025-01-10 15:38:04 -0600

    adds calendar WIP

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import { FadingTextProvider } from '../contexts/FadingTextContext';
 import Landing from './landing';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Calendar from './calendar/calendar';
 
 export default function Home() {
   return (
--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -21,3 +21,27 @@ export const getFromLocalStorage = (key: string): string => {
   }
   return '';
 };
+
+export const addSleepToLocalStorage = (date: string, score: number): void => {
+  if (global?.window !== undefined) {
+    let existing: Record<string, any> = JSON.parse(
+      localStorage.getItem('sleep') || '{}',
+    );
+
+    existing[date] = score;
+    localStorage.setItem('sleep', JSON.stringify(existing));
+  }
+};
+
+export const getSleepScoreFromLocalStorage = (date: string): number => {
+  if (global?.window !== undefined) {
+    const existing: Record<string, any> = JSON.parse(
+      localStorage.getItem('sleep') || '{}',
+    );
+
+    if (existing && existing.hasOwnProperty(date)) {
+      return existing[date];
+    }
+  }
+  return 0;
+};
